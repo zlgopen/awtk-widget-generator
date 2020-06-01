b3db4aaa92e24794a9aa9c681a293f4f375788b7
lixianjing <xianjimli@hotmail.com>
2020-06-01 16:53:21 +0800

    add template

---
+++ template/README.md
@@ -0,0 +1,24 @@
+# awtk-widget-template
+
+template 控件。
+ 
+![](docs/images/ui.jpg)
+
+## 准备
+
+1. 获取 awtk 并编译
+
+```
+git clone https://github.com/zlgopen/awtk.git
+cd awtk; scons; cd -
+```
+
+## 运行
+
+```
+./bin/demo_template
+```
+
+## 文档
+
+[AWTK 自定义控件规范](https://github.com/zlgopen/awtk/blob/master/docs/custom_widget_rules.md)
---
+++ template/demos/main.c
@@ -0,0 +1,45 @@
+/**
+ * File:   main.c
+ * Author: AWTK Develop Team
+ * Brief:  main window sources
+ *
+ * Copyright (c) 2020 - 2020  Guangzhou ZHIYUAN Electronics Co.,Ltd.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * License file for more details.
+ *
+ */
+
+/**
+ * History:
+ * ================================================================
+ * 2020-05-29 Li XianJing <xianjimli@hotmail.com> created
+ *
+ */
+
+#include "awtk.h"
+
+static ret_t on_close(void* ctx, event_t* e) {
+  tk_quit();
+
+  return RET_OK;
+}
+
+ret_t application_init() {
+  template_register();
+
+  widget_t* win = window_open("main");
+
+  widget_child_on(win, "close", EVT_CLICK, on_close, NULL); 
+
+  return RET_OK;
+}
+
+ret_t application_exit() {
+  return RET_OK;
+}
+
+#include "awtk_main.inc"
+
